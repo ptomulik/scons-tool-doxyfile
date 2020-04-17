163ef5bfcf0927466b77fa8a31a4fcd973635615
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-04-17 10:30:08 +0200

    Fixed issue #2

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ Tool specific initialization for doxyfile.
 """
 
 #
-# Copyright (c) 2013-2018 by Paweł Tomulik
+# Copyright (c) 2013-2020 by Paweł Tomulik
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
